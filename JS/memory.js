4dd5ebf3811336198fe866e7043ef3d8a2b42b91
Axel <axelpriven@gmail.com>
2018-11-13 15:59:18 +0200

    add

---
+++ JS/memory.js
@@ -0,0 +1,9 @@
+function startGame() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("game").style.display = "block";
+}
+
+function aboutUs() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("aboutGame").style.display = "block";
+}
