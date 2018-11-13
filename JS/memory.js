0e82e53da66153ea2fa0a2d202e0140a0e5e4154
Axel <axelpriven@gmail.com>
2018-11-13 18:24:39 +0200

    add more

--- JS/memory.js
+++ JS/memory.js
@@ -7,3 +7,8 @@ function aboutUs() {
     document.getElementById("intro").style.display = "none";
     document.getElementById("aboutGame").style.display = "block";
 }
+document.getElementsByClassName("card").addEventListener("click", flipCard)
+
+function flipCard(){
+    document.getElementsByClassName("card").style.display = "none";
+}
